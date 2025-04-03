b8d2ffd314fdbb543b7f6a823795b2a92b202c8f
smith478 <dsmith.smith864@gmail.com>
2025-04-02 22:26:08 -0400

    Updating run_fastrtc.sh to open docker container for more flexibility

--- audio_transcription/run_fastrtc.sh
+++ audio_transcription/run_fastrtc.sh
@@ -15,7 +15,7 @@ fi
 # Get the host's IP address for network access
 HOST_IP=$(hostname -I | awk '{print $1}')
 echo "Your host IP is: $HOST_IP"
-echo "You can access the application at http://$HOST_IP:7860"
+echo "After running fastrtc/whisper_remote.py you can access the application at http://$HOST_IP:7860"
 
 # Create the HuggingFace cache directory if it doesn't exist
 HF_CACHE_DIR="$HOME/.cache/huggingface"
@@ -28,5 +28,5 @@ docker run $GPU_FLAG \
     -p 7860:7860 \
     -v $HF_CACHE_DIR:/root/.cache/huggingface \
     -v $(pwd):/phi4-audio-transcriber \
-    phi4-audio-transcriber:latest \
-    python /phi4-audio-transcriber/fastrtc/whisper_local.py
+    phi4-audio-transcriber:latest # \
+    # python /phi4-audio-transcriber/fastrtc/whisper_remote.py
