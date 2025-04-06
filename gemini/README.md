b73dbdb4d4d217fcf227cbe266dabe17c7bb7839
smith478 <dsmith.smith864@gmail.com>
2025-04-06 10:07:10 -0400

    Adding small test of gemini api

---
+++ gemini/README.md
@@ -0,0 +1,7 @@
+This is a small test of the Gemini API. Note that the following package is required:
+
+```bash
+pip install google-genai
+```
+
+Also here there is a `.env` file in the `/gemini` directory with `GEMINI_API_KEY`
---
+++ gemini/test.py
@@ -0,0 +1,15 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+prompt = "Explain the concept of Occam's Razor and provide a simple, everyday example."
+response = client.models.generate_content(
+    model="gemini-2.5-pro-exp-03-25",  # or gemini-2.0-flash-thinking-exp
+    contents=prompt
+)
+
+print(response.text)
